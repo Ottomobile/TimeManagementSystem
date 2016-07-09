8b77bcb06924ae2cb73c532770954963ff0386e2
Otto Lau <otto.lau@hotmail.com>
2016-07-09 15:31:25 -0700

    Validate that ManagingUsers exists and is a manager

--- src/TimeManagementSystem/Controllers/SubscribeToUsersController.cs
+++ src/TimeManagementSystem/Controllers/SubscribeToUsersController.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TimeManagementSystem.Data;
 using TimeManagementSystem.Models;
+using System.Data;
 
 namespace TimeManagementSystem.Controllers
 {
@@ -58,9 +56,33 @@ namespace TimeManagementSystem.Controllers
             if (ModelState.IsValid)
             {
                 subscribeToUser.CurrentUser = this.User.Identity.Name;
-                _context.Add(subscribeToUser);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
+
+                // Determine if the managing user exists and is a manager
+                bool isManagingUser = false;
+                var connection = _context.Database.GetDbConnection();
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM [dbo].[ManagersView] WHERE [ManagerUserName] = @managerUserName";
+                    IDbDataParameter managerParam = command.CreateParameter();
+                    managerParam.ParameterName = "@managerUserName";
+                    managerParam.Value = subscribeToUser.ManagingUser;
+                    command.Parameters.Add(managerParam);
+                    var result = command.ExecuteReader();
+                    isManagingUser = result.HasRows;
+                }
+                connection.Close();
+
+                if (isManagingUser) {
+                    _context.Database.GetDbConnection().Open();
+                    _context.Add(subscribeToUser);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "User entered is not a Manager.");
+                }
             }
             return View(subscribeToUser);
         }
